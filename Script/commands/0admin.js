eb80c696ba1ffe2f3c76cc0cbe4374e08021e5e4
zoro-vex220 <zorovex2.20@gmail.com>
2026-06-26 11:51:19 +0600

    Update 0admin.js

--- Script/commands/0admin.js
+++ Script/commands/0admin.js
@@ -5,7 +5,7 @@ module.exports.config = {
 	name: "0admin",
 	version: "1.0.5",
 	hasPermssion: 3,
-	credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
+	credits: "🔰Zoro Vex🔰",
 	description: "Admin Config",
 	commandCategory: "Admin",
 	usages: "Admin [command] [@mention/reply/UID/link/name]",
